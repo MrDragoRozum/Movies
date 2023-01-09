0df276cbeab711b08f9987ad1f44b7b5dea81e6e
Александр <107046079+MrDragoRozum@users.noreply.github.com>
2023-01-09 14:58:22 +0300

    Добавлены изменения

---
+++ app/src/main/java/com/example/movies/ApiService.java
@@ -0,0 +1,10 @@
+package com.example.movies;
+
+import io.reactivex.rxjava3.core.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiService {
+    @GET("movie?token=ZQQ8GMN-TN54SGK-NB3MKEC-ZKB8V06&field=rating.kp&search=7-10&sortField=votes.imdb&sortType=-1")
+    Single<MovieResponse> loadMovies(@Query("page") int page);
+}
--- app/src/main/java/com/example/movies/MainActivity.java
+++ app/src/main/java/com/example/movies/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.movies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MainViewModel mainViewModel;
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
+        mainViewModel.getMovies().observe(this, movies -> Log.d(TAG, movies.toString()));
+        mainViewModel.loadMovies();
     }
 }
