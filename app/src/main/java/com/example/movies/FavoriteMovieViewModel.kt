6bbfc7e591367da97f9d7570575088dbbc7286a2
Александр <107046079+MrDragoRozum@users.noreply.github.com>
2023-06-05 14:04:54 +0300

    Реализован FavoriteMovieViewModel

--- app/src/main/java/com/example/movies/FavoriteMovieViewModel.kt
+++ app/src/main/java/com/example/movies/FavoriteMovieViewModel.kt
@@ -1,4 +1,11 @@
 package com.example.movies
 
-class FavoriteMovieViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.example.movies.database.MovieDatabase
+
+class FavoriteMovieViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val movieDao = MovieDatabase.getInstance(application).movieDao()
+    val getAllFavoriteFilms get() = movieDao.getAllFavoriteMovies()
 }
